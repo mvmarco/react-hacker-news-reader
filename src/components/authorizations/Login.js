627508a68f864ee39a0e1dddee7fcca87d59b71f
marco corapi <marco.corapi@outlook.it>
2021-05-28 23:13:47 +0200

    form and login hook state

--- src/components/authorizations/Login.js
+++ src/components/authorizations/Login.js
@@ -1,17 +1,55 @@
+import React from "react";
+import useFormValidation from "./useFormValidation";
+
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  password: "",
+};
+
 function Login(props) {
+  const { handleSubmit, handleChange, values } = useFormValidation(INITIAL_STATE);
+  // state
+  const [login, setLogin] = React.useState(true); //input hidden if it is true
   return (
     <div className="login-section">
-      <h2 className="login-title">Create Account</h2>
-      <form className="login-section">
-        <input type="text" placeholder="Your Name" autoComplete="off" />
-        <input type="email" placeholder="Your Email" autoComplete="off" />
-        <input type="passwod" placeholder="Choose a Secure Password" />
+      <h2 className="login-title">{login ? "Login" : "Create Account"}</h2>
+      <form onSubmit={handleSubmit} className="login-section">
+        {!login && (
+          <input
+            name="name"
+            values={values.name}
+            onChange={handleChange}
+            type="text"
+            placeholder="Your Name"
+            autoComplete="off"
+          />
+        )}
+        <input
+          name="email"
+          values={values.email}
+          onChange={handleChange}
+          type="email"
+          placeholder="Your Email"
+          autoComplete="off"
+        />
+        <input
+          name="password"
+          values={values.password}
+          onChange={handleChange}
+          type="password"
+          placeholder="Choose a Secure Password"
+        />
         <div>
           <button type="submit" className="submit-button">
             Submit
           </button>
-          <button type="submit" className="account-button">
-            Already have an account?
+          <button
+            type="submit"
+            className="account-button"
+            onClick={() => setLogin((prevLogin) => !prevLogin)}
+          >
+            {login ? "Need to create an account?" : "Already have an account?"}
           </button>
         </div>
       </form>
--- src/components/authorizations/useFormValidation.js
+++ src/components/authorizations/useFormValidation.js
@@ -1,3 +1,21 @@
-function useFormValidation() {}
+import { useState } from "react";
+
+function useFormValidation(initialState) {
+  const [values, setValues] = useState(initialState);
+
+
+  function handleChange(event) {
+    setValues(previousValues => ({
+      ...previousValues,
+      [event.target.name] : event.target.value
+    }))
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log({values})
+  }
+  return { handleChange, handleSubmit, values}
+}
 
 export default useFormValidation;
