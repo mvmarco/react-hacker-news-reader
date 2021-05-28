006d0a166321ac0308a00e1221a665780a0d80ff
marco corapi <marco.corapi@outlook.it>
2021-05-28 22:21:12 +0200

    buttons and form

--- src/components/authorizations/Login.js
+++ src/components/authorizations/Login.js
@@ -1,5 +1,22 @@
 function Login(props) {
-  return <div>Login</div>;
+  return (
+    <div className="login-section">
+      <h2 className="login-title">Create Account</h2>
+      <form className="login-section">
+        <input type="text" placeholder="Your Name" autoComplete="off" />
+        <input type="email" placeholder="Your Email" autoComplete="off" />
+        <input type="passwod" placeholder="Choose a Secure Password" />
+        <div>
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+          <button type="submit" className="account-button">
+            Already have an account?
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
